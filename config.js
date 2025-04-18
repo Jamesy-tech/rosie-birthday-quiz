447e580000deefeddea316e807b0266011000a79
jamesy-tech <jamesycreator@gmail.com>
2025-04-18 14:04:56 +1200

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+var quiz_name = "Rosalie's Birthday Quiz";
+var quiz_image_url = "images/shapes.png";
+var quiz_end_image_url = "images/stacked_shapes.png";
+var quiz_description = "Look up at the screen for the questions and write the letter for the correct answer on your sheet.";
+var quiz_end_description = "Made with Jamesy Creator";
+var quiz_author = "James Trendall";
+
+var questions = [
+    { text: "How old is Rosalie?", options: ["11", "12", "13"], correctAnswer: "12", correctAnswerLetter: "B" },
+    { text: "What is Rosalie's second favourite animal?", options: ["Woolly Mammoth", "Giraffe", "Tiger"], correctAnswer: "Giraffe", correctAnswerLetter: "B" },
+    { text: "Who is Rosalie's favourite sports person?", options: ["Alexander Trusova Smith", "Hiroki Sakai", "Crisiano Ronaldo"], correctAnswer: "Alexander Trusova", correctAnswerLetter: "A" },
+    { text: "What is Rosalie's second/middle name?", options: ["Priscilla", "Stephanie", "Isabella"], correctAnswer: "Stephanie", correctAnswerLetter: "C" },
+    { text: "What is Rosalie's favourite TV program?", options: ["The News", "One Tree Hill", "Gossip Girl"], correctAnswer: "Gossip Girl", correctAnswerLetter: "C" },
+    { text: "What is Rosalie's favourite movie?", options: ["Time Cut", "Elevator Game", "Lion King"], correctAnswer: "Time Cut", correctAnswerLetter: "A" },
+    { text: "What is Rosalie's favourite song?", options: ["Espresso", "Busy Woman", "New Zealand National Anthem"], correctAnswer: "Busy Woman", correctAnswerLetter: "B" },
+    { text: "What is Rosalie's favourite takeaway food?", options: ["St-Pierres Sushi", "McDonald's", "KFC"], correctAnswer: "KFC", correctAnswerLetter: "C" },
+    { text: "What is Rosalie's favourite subject at school?", options: ["Physical Education", "Reading", "Writing"], correctAnswer: "Physical Education", correctAnswerLetter: "A" },
+    { text: "What is Rosalie's dad's first name?", options: ["James", "Simon", "John"], correctAnswer: "James", correctAnswerLetter: "A" },
+    { text: "How many months old is Rosalie?", options: ["120", "132", "144"], correctAnswer: "144", correctAnswerLetter: "C" },
+    { text: "What is Rosalie's favourite activity?", options: ["Watch/play Football", "Ice Skating", "Homework"], correctAnswer: "Ice Skating", correctAnswerLetter: "B" },
+     
+];
+
